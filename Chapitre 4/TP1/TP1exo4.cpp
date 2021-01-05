8ba42b6260bfaa13000113c09d68a16a3b67f617
MaelDrelon <mael.drelon1@gmail.com>
2021-01-05 17:21:45 +0100

    upd

--- Chapitre 4/TP1/TP1exo4.cpp
+++ Chapitre 4/TP1/TP1exo4.cpp
@@ -1,27 +1,50 @@
-#include <iostream>
-void affiche (int tab1[20], int tab2[20],int i,int j)
+#include <stdio.h>
+#include <stdlib.h>
+
+void affiche(int * tab, int line, int col)
 {
-	for (i=0; i<20; i++)
-        printf ("\n%d = %d\n",i, tab1[i]);
-
-    printf("\n\n");
-
-	for (j=0; j<20; j++)
-        printf ("\n%d = %d\n",j, tab2[j]);
+	int i, j;
+	for (i = 0; i < line; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf("%4d, ", tab[(i * col) + j]);
+		}
+		printf("\n");
+	}
 }
 
 
+int main(int argc, char ** argv)
+{
+	int tab1[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19 };
+	int tab2[] = { -19,18,-17,16,-15,14,-13,12,-11,10,-9,8,-7,6,-5,4,-3,2,-1,0 };
+	int i, j;
 
+	int sizeTab1 = sizeof(tab1) / sizeof(int);
+	int sizeTab2 = sizeof(tab2) / sizeof(int);
 
-int main(int argc, char** argv) {
+	int * result = (int*)malloc(sizeof(tab1) * sizeof(tab2));
+	system("mode con lines=50 cols=130");
 
-	int tab1[20]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-	int tab2[20]={-19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2,-1,0};
-	int	i;
-	int j;
+	printf("sizeof(tab1) => %d\n", sizeof(tab1));	// 80
+	printf("sizeof(tab2) => %d\n", sizeof(tab2));	// 80
+	printf("sizeof(int) => %d\n", sizeof(int));		// 4
+	printf("sizeTab1 => %d\n", sizeTab1);			// 20
+	printf("sizeTab2 => %d\n", sizeTab2);			// 20
+	printf("sizeof(result) => %d\n", sizeof(result)); // 1600 ??? Non ! => 4 (taille en mémoire d'un pointeur)
 
-	affiche(tab1, tab2, i, j);
+	for (i = 0; i < sizeTab1; i++)
+	{
+		for (j = 0; j < sizeTab2; j++)
+		{
+			result[(i * sizeTab2) + j] = tab1[i] * tab2[j];
+		}
+	}
 
+	affiche(result, sizeTab1, sizeTab2);
+
+	free(result);
 
 	return 0;
 }
